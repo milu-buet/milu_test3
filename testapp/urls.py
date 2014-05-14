1dd788a7d3074274f937b36e690f47cca2a07401
milu-buet <milu.buet@gmail.com>
2014-05-14 19:29:07 +0600

    test commit 1

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,1 +1,12 @@
 __author__ = 'milu'
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'testapp.views.home'),
+    # url(r'^blog/', include('blog.ur
+)
