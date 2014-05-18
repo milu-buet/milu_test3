df37c03fb8e5c05c6d16d41910d94810c5e5c3a8
milu-buet <milu.buet@gmail.com>
2014-05-18 20:57:43 +0600

    test commit 1

---
+++ dajaxtest/ajax.py
@@ -0,0 +1,8 @@
+__author__ = 'milu'
+
+from django.utils import simplejson
+from dajaxice.decorators import dajaxice_register
+
+@dajaxice_register
+def sayhello(request):
+    return simplejson.dumps({'message':'Hello World'})
